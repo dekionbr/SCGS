af60510119a76df007082b97cd223987d2a8eee1
Julio Cesar Silveira <silveira.julio@gmail.com>
2017-08-03 02:27:36 -0300

    up

---
+++ SCGS.WEB.Tests/Controllers/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SCGS.WEB.Controllers;
+using System.Web.Mvc;
+
+namespace SCGS.WEB.Tests.Controllers
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+            RelatoriosController controller = new RelatoriosController();
+
+            // Act
+            JsonResult result = controller.Funcionarios(4) as JsonResult;
+
+            Assert.IsTrue(result.Data != null, "Foi");
+        }
+    }
+}
